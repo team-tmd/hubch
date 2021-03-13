ae54140915fecac2701480a02222a87b19c7bbcd
tomotakashimizu <tomotaka.developer@gmail.com>
2021-03-13 18:30:38 +0900

    Firebase JavaScript SDK の初期設定

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase"
+import "firebase/firestore"
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAIFPsNzoNoBq2pOu6azXodTfx26WCXkfY",
+  authDomain: "hubch-d8ab5.firebaseapp.com",
+  projectId: "hubch-d8ab5",
+  storageBucket: "hubch-d8ab5.appspot.com",
+  messagingSenderId: "1035855594989",
+  appId: "1:1035855594989:web:f7b23c0727d9b3d3e793e4",
+  measurementId: "G-YHLV0FWMRY",
+}
+
+firebase.initializeApp(firebaseConfig)
