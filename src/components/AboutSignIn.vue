b20fefda90554bc3281165c32975f1fc73dd37df
tomotakashimizu <tomotaka.developer@gmail.com>
2021-03-29 16:13:28 +0900

    About ページに SignIn の仕方を作成

---
+++ src/components/AboutSignIn.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <br />
+    <h1>【HubCh の始め方】</h1>
+    <br />
+    <h2>サインイン</h2>
+    <h3>1. 右上のSignInをクリック</h3>
+    <h3>2. Googleログインボタンをクリック</h3>
+    <img
+      src="/images/about_page/signin-page.png"
+      alt="About Sign in"
+      v-on:click="toSignInPage"
+      class="about-image"
+      id="about-signin-image"
+    />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.about-image {
+  width: 700px;
+  max-width: 100%;
+}
+</style>
